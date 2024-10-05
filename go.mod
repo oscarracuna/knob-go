242e8a28ac577dc9a2149595aca5b542f49bce28
oscarracuna <acunainfosec@proton.me>
2024-10-05 10:43:09 -0600

    mod and a few things

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oscarracuna/knob-go
+
+go 1.22.5
--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
   "fmt"
+  "os"
 )
 
 func main() {
   fmt.Println("ola")
+  //This is for my specific keeb
+  //Went for symlink instead of event file
+  file, err := os.Open("/dev/input/by-id/usb-Telink_Wireless_Gaming_Keyboard-event-kbd")
+  if err != nil {
+    fmt.Println("Something went wrong with the file: ", err)
+    return
+  }
+  defer file.Close()
 }
+
+// Event thingy
+// /dev/input/by-id/usb-Telink_Wireless_Gaming_Keyboard-event-kbd
+
